df493136cace1a0a175bcafe751c3fd952451f53
mohammad-AL-fayoumi <157965052+mohammad-AL-fayoumi@users.noreply.github.com>
2026-06-13 01:38:13 +0300

    Update BookAppointment.vue

--- views/BookAppointment.vue
+++ views/BookAppointment.vue
@@ -47,7 +47,7 @@ export default {
     };
   },
   mounted() {
-    fetch("https://e2m2b7y8c9.execute-api.us-east-1.amazonaws.com/prod/slots")
+      fetch("https://fw1ge93h1a.execute-api.us-east-1.amazonaws.com/slots")
       .then(res => res.json())
       .then(data => {
         const parsed = JSON.parse(data.body);
@@ -62,7 +62,7 @@ export default {
         slot: this.selectedSlot
       };
 
-      fetch("https://e2m2b7y8c9.execute-api.us-east-1.amazonaws.com/prod/appointments", {
+        fetch("https://fw1ge93h1a.execute-api.us-east-1.amazonaws.com/appointments", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ body: JSON.stringify(payload) })
